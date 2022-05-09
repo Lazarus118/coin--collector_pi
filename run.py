ce420f7fbf62be8e2a27d09cd020e5b3553607b1
Austin Lazarus <austin.lazarus@gmail.com>
2022-05-08 21:41:56 -0400

    initial commit

--- run.py
+++ run.py
@@ -7,9 +7,8 @@ import tm1637
 # Initialize the display (GND, VCC=3.3V, Example Pins are DIO-20 and CLK21)
 Display = tm1637.TM1637(CLK=23, DIO=24, brightness=1.0)
 
-while True:
+while (True):
     digits = [0, 1, 7, 5]
     Display.Show(digits)
     Display.ShowDoublepoint(True)
     sleep(0.2)
-    Display.cleanup()
